702bad9cece9e85a9fb1e739e9dc36b8268ea598
Devildk384 <durgeshkumar384@gmail.com>
2021-01-28 12:00:28 +0530

    first commit

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -6,6 +6,11 @@ import PhotoSave from "../../images/portfolio/photosave.png"
 import FinalPractice from "../../images/portfolio/finalpractice.png"
 import MovieInfo from "../../images/portfolio/movieinfo.jpg"
 import ShopToBuy from "../../images/portfolio/shoptobuy.png"
+import movieInfoApp from "../../images/portfolio/MovieInfo-App.png"
+import linkedin from "../../images/portfolio/linkedIn.png"
+import snapchat from "../../images/portfolio/snapchat.png"
+
+
 
 
 import "./work.scss"
@@ -100,7 +105,7 @@ const Work = () => {
             </ul>
 
             <div className="project__links">
-            
+             
               <a
                 
                 href="http://35.202.180.243/"
@@ -135,7 +140,7 @@ const Work = () => {
         <div className="project movieinfo-project">
           <figure className="project__img-wrapper">
             <a
-              href="https://cranky-babbage-15aa75.netlify.app/"
+              href="https://movieinfo-d1449.web.app/"
               rel="noopener noreferrer"
               target="_blank"
             >
@@ -173,7 +178,7 @@ const Work = () => {
               </a>
               <a
                 
-                href="https://cranky-babbage-15aa75.netlify.app/"
+                href="https://movieinfo-d1449.web.app/"
 
                 rel="noopener noreferrer"
                 target="_blank"
@@ -187,6 +192,66 @@ const Work = () => {
           </div>
         </div>
 
+
+        
+
+        <div className="project shoptobuy-project">
+
+        <div className="project__info">
+            <h5>Featured Project</h5>
+            <h4>LinkedIn Clone</h4>
+
+            <div className="project__about">
+              <p>
+              A linkedIn clone  using React, Redux , firebase for using authentication, database, hosting. Using material-ui for icons
+              </p>
+            </div>
+
+            <ul>
+             <li>React</li>
+              <li>Redux</li>
+              <li>firebase</li>
+              <li>Css</li>
+            </ul>
+
+            <div className="project__links">
+              <a
+                href="https://github.com/Devildk384/linkedIn-clone"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <FaGithub className="project__icon" title="Github" />
+              </a>
+              <a
+                href="https://linkedin-clone-eea1b.web.app/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <FaExternalLinkAlt
+                  className="project__icon"
+                  title="View Project"
+                />
+              </a>
+            </div>
+          </div>
+
+          <figure className="project__img-wrapper">
+            <a
+              href="https://linkedin-clone-eea1b.web.app/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+             <img className="project__img" alt="linkdin-clone hero" src={linkedin} />
+             
+            </a>
+          </figure>
+
+         
+        </div>
+
+        
+
+       
         <div className="project covid-project">
           <figure className="project__img-wrapper">
             <a
@@ -238,39 +303,98 @@ const Work = () => {
           </div>
         </div>
 
-        <div className="project photosave-project">
+
+ <div className="project shoptobuy-project">
+
+ <div className="project__info">
+    <h5>Featured Project</h5>
+    <h4>Snapchat Clone</h4>
+
+    <div className="project__about">
+      <p>
+      A Snapchat clone  using React, Redux , firebase for using authentication, database, hosting. Using material-ui for icons
+      </p>
+    </div>
+
+    <ul>
+     <li>React</li>
+      <li>Redux</li>
+      <li>firebase</li>
+      <li>Css</li>
+    </ul>
+
+    <div className="project__links">
+      <a
+        href="https://github.com/Devildk384/snapchat-clone"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <FaGithub className="project__icon" title="Github" />
+      </a>
+      <a
+        href="https://snapchat-clone-41e7c.web.app"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <FaExternalLinkAlt
+          className="project__icon"
+          title="View Project"
+        />
+      </a>
+    </div>
+  </div>
+
+  <figure className="project__img-wrapper">
+    <a
+      href="https://snapchat-clone-41e7c.web.app"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+     <img className="project__img" alt="snapchat-clone hero" src={snapchat} />
+     
+    </a>
+  </figure>
+
+ 
+</div>
+
+
+
+        <div className="project movieInfoApp-project">
           <figure className="project__img-wrapper">
             <a
-              href="https://play.google.com/store/apps/details?id=com.AD.photosave"
+              href="https://play.google.com/store/apps/details?id=com.AD.movieinfo"
               rel="noopener noreferrer"
               target="_blank"
             >
 
-          <img className="project__img" alt="PhotoSave hero" src={PhotoSave} />  
+          <img className="project__img" alt="PhotoSave hero" src={movieInfoApp} />  
              
             </a>
           </figure>
 
           <div className="project__info">
             <h5>Featured Project</h5>
-            <h4>PhotoSave</h4>
+            <h4>MovieInfo</h4>
 
             <div className="project__about">
               <p>
-              A PhotoSave app built in flutter. Fetching photo from API. Easy to Download photos on onclick
+              A MovieInfo app built in flutter. Fetching data from TMDB API. Details information about your favorite Movies, 
+               actors, recently added movies, and detailed information about each movie. Also check out their trailers.
               </p>
             </div>
 
             <ul>
               <li>Flutter</li>
               <li>Dart</li>
+              <li>TMDB API</li>
            
               
             </ul>
 
             <div className="project__links">
               <a
-                href="https://play.google.com/store/apps/details?id=com.AD.photosave"
+                href="https://play.google.com/store/apps/details?id=com.AD.movieinfo"
                 rel="noopener noreferrer"
                 target="_blank"
               >
@@ -282,6 +406,8 @@ const Work = () => {
             </div>
           </div>
         </div>  
+
+       
 
         <div className="project finalpractice-project">
           <figure className="project__img-wrapper">
@@ -326,7 +452,52 @@ const Work = () => {
               </a>
             </div>
           </div>
-        </div>      
+        </div> 
+
+         <div className="project photosave-project">
+          <figure className="project__img-wrapper">
+            <a
+              href="https://play.google.com/store/apps/details?id=com.AD.photosave"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+
+          <img className="project__img" alt="PhotoSave hero" src={PhotoSave} />  
+             
+            </a>
+          </figure>
+
+          <div className="project__info">
+            <h5>Featured Project</h5>
+            <h4>PhotoSave</h4>
+
+            <div className="project__about">
+              <p>
+              A PhotoSave app built in flutter. Fetching photo from API. Easy to Download photos on onclick
+              </p>
+            </div>
+
+            <ul>
+              <li>Flutter</li>
+              <li>Dart</li>
+           
+              
+            </ul>
+
+            <div className="project__links">
+              <a
+                href="https://play.google.com/store/apps/details?id=com.AD.photosave"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <FaExternalLinkAlt
+                  className="project__icon"
+                  title="View Project"
+                />
+              </a>
+            </div>
+          </div>
+        </div>       
 
       </div>
     </section>
